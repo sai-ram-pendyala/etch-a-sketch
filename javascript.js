096615d901615752520d6e9d7b7a51cb75b4b73c
sai-ram-pendyala <89296568+sai-ram-pendyala@users.noreply.github.com>
2023-02-15 10:33:53 +0530

    Add mouse events on grid

--- javascript.js
+++ javascript.js
@@ -1,14 +1,29 @@
 let grid = document.createElement('div');
 grid.className = 'grid';
-for(let i = 0; i < 16; i++) {
-    let row = document.createElement('div');
-    row.className = 'row';
-    for(let j = 0; j < 16; j++) {
+for(let i = 0; i < 100; i++) {
+    let column = document.createElement('div');
+    column.className = 'column';
+    for(let j = 0; j < 60; j++) {
         let cell = document.createElement('div');
         cell.className = 'cell';
-        row.appendChild(cell);
+        column.appendChild(cell);
     }
-    grid.appendChild(row);
+    grid.appendChild(column);
 }
-
 document.body.appendChild(grid);
+
+let down = false;
+const cells = document.querySelectorAll('.cell');
+document.addEventListener('mousedown', function() {
+    down = true;
+})
+
+document.addEventListener('mouseup', function() {
+    down = false;
+})
+
+cells.forEach(cell => cell.addEventListener('mouseover', function(e) {
+    if(down == false)
+        return;
+    e.target.classList.add('hover');
+}))
