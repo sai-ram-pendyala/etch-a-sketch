e2520298ce275f399b92c838a7e7dc94f50cbac7
sai-ram-pendyala <89296568+sai-ram-pendyala@users.noreply.github.com>
2023-02-23 16:34:33 +0530

    Add hover event to grid

--- javascript.js
+++ javascript.js
@@ -3,11 +3,11 @@ const rowsValue = document.getElementById('rowsValue');
 const grid = document.getElementById('grid');
 
 rowsValue.textContent = rowsRange.value;
+let clicked = false;
 
 function createGrid(rows) {
     grid.innerHTML = '';
-    const cellSize = 900/rows;
-    console.log(cellSize);
+    const cellSize = 640/rows;
     let totalCells = rows*rows;
     while (totalCells--) {
         const cell = document.createElement('div');
@@ -15,12 +15,31 @@ function createGrid(rows) {
         cell.style.width = `${cellSize}px`;
         cell.className = 'cell';
         grid.appendChild(cell);
+
+        cell.addEventListener('mousedown', function() {
+            cell.classList.add('hover');
+            document.addEventListener('mousemove', hover);
+        });
     }
 }
 
+
 createGrid(rowsRange.value);
+
+const cells = document.querySelectorAll('.cell');
 
 rowsRange.addEventListener('input', function() {
     rowsValue.textContent = this.value;
     createGrid(this.value);
 });
+
+document.addEventListener('mouseup', function() {
+    document.removeEventListener('mousemove', hover);
+});
+
+function hover(evt) {
+    if(!grid.contains(evt.target)) return; 
+    evt.target.classList.add('hover');  
+}
+
+
